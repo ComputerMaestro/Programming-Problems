6b5359fa8df9a4086dc894897cb012a6d8b22672
Yash Patel <yp270200@gmail.com>
2021-01-04 11:15:04 +0530

    fixed gitignore

--- Python/calculate_elapsed_time.py
+++
@@ -1,9 +0,0 @@
-import time
-a = [0]*5
-t0 = time.perf_counter()
-c = [2+x for x in a]
-t1 = time.perf_counter()
-b = list(map(lambda x: x+2, a))
-t2 = time.perf_counter()
-print('1', t1-t0)
-print('2', t2-t1)
--- Python/minimum_cut.py
+++
@@ -1,6 +0,0 @@
-import random
-
-input_graph = open('C:/Users/Computer Maestro/Desktop/practice/karger_input.txt').read().split('\n')[:-1]
-input_graph = [x.split() for x in input_graph]
-
-print(input_graph[1], input_graph[122])
