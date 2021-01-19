3b0be60da775573ac4f7a2df93c531f859f07729
Yash Patel <yp270200@gmail.com>
2021-01-19 15:52:37 +0530

    dp question

---
+++ C++/geeks/shortest_common_supersequence.cpp
@@ -0,0 +1,55 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int shortestCommonSupersequence(char* , char*, int, int );
+
+int main()
+{   
+    
+    int t;
+    char X[10001], Y[10001];
+    
+    //taking total testcases
+    cin >> t;
+    while(t--){
+    
+    //taking String X and Y
+	cin >> X >> Y;
+	
+	//calling function shortestCommonSupersequence()
+	cout << shortestCommonSupersequence(X, Y, strlen(X), strlen(Y))<< endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
+
+
+//User function template for C++
+
+// X : 1st given string of size m
+// Y : 2nd given string of size n
+int shortestCommonSupersequence(char* X, char* Y, int m, int n) {
+    if(m == 0) {return n;}
+    else if(n == 0) {return m;}
+    
+    int L[m + 1][n + 1];
+ 
+    for (int i = 0; i <= m; i++) {
+        for (int j = 0; j <= n; j++) {
+            if (i == 0 || j == 0)
+                L[i][j] = 0;
+ 
+            else if (X[i - 1] == Y[j - 1])
+                L[i][j] = L[i - 1][j - 1] + 1;
+ 
+            else
+                L[i][j] = max(L[i - 1][j], L[i][j - 1]);
+        }
+    }
+ 
+    return m+n-L[m][n];
+}
